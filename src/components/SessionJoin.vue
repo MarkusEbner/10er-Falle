0e7a51f289c066f9abcdbc9046fce4cbb0b2080d
s368952 <markus.ebner@stud-mail.uni-wuerzburg.de>
2024-12-05 19:08:06 +0100

    Display sessionId

--- src/components/SessionJoin.vue
+++ src/components/SessionJoin.vue
@@ -1,14 +1,22 @@
 <template>
   <v-container>
+    <h1>Join Session {{ sessionId }} </h1>
+    <p> </p>
     <v-text-field v-model="name" label="Dein Name"></v-text-field>
     <v-btn @click="joinSession">Session beitreten</v-btn>
   </v-container>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const sessionId = route.params.sessionId
 
 const name = ref('');
+
 const joinSession = () => {
   if (name.value) {
     alert(`Willkommen, ${name.value}!`);
