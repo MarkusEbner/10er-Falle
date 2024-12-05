cc667b036a735f81b3944d0203d0d78f7d73c647
s368952 <markus.ebner@stud-mail.uni-wuerzburg.de>
2024-12-05 18:41:37 +0100

    Initial commit

---
+++ src/components/GameMenu.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-btn @click="startGame">Neue 10er Falle Session starten</v-btn>
+    <v-divider/>
+    <v-text-field v-model="sessionLink" label="Session-Link" readonly></v-text-field>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const sessionLink = ref('');
+const startGame = () => {
+  sessionLink.value = `https://10er-falle.vercel.app/join/${Math.random()
+    .toString(36)
+    .substr(2, 8)}`;
+};
+</script>
---
+++ src/components/SessionJoin.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-text-field v-model="name" label="Dein Name"></v-text-field>
+    <v-btn @click="joinSession">Session beitreten</v-btn>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const name = ref('');
+const joinSession = () => {
+  if (name.value) {
+    alert(`Willkommen, ${name.value}!`);
+  } else {
+    alert('Bitte gib einen Namen ein.');
+  }
+};
+</script>
