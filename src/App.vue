f6cbc45854d288ab8607b0fa1533e61866425fe0
s368952 <ebner.markus33@t-online.de>
2025-01-22 13:47:48 +0100

    Add speed insights

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
   <v-app>
     <v-main>
       <router-view />
+      <SpeedInsights />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+  import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
